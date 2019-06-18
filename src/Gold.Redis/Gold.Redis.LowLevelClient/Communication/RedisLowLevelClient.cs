dcfc21d025207e1629d8d181ae2591352947bc9a
Tomer Goldberg <tomer.gold.a@gmail.com>
2019-06-18 07:32:09 +0300

    Created the low level Redis protocol usage - TODO: still need to clean up the code and think about how to use Request to be RedisRequests And Response to be RedisResponse

---
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/RedisLowLevelClient.cs
@@ -0,0 +1,69 @@
+﻿using Gold.Redis.Common.Interfaces.Communication;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gold.Redis.LowLevelClient.Communication
+{
+    public class RedisLowLevelClient : IRedisConnection
+    {
+        private readonly IConnectionsContainer _connections;
+
+        public RedisLowLevelClient(IConnectionsContainer connections)
+        {
+            _connections = connections;
+        }
+
+        public async Task<string> ExecuteCommand(string command)
+        {
+            var socket = await _connections.GetSocket();
+            try
+            {
+                var bytes = Encoding.ASCII.GetBytes(command);
+                var bytesAsArraySegment = new ArraySegment<byte>(bytes);
+                var sendResult = await socket.SendAsync(bytesAsArraySegment, SocketFlags.None);
+                using (var bufferStream = new BufferedStream(new NetworkStream(socket), 16 * 1024))
+                {
+                    return ReadResponseFromStream(bufferStream);
+                }
+            }
+            finally
+            {
+                _connections.FreeSocket(socket);
+            }
+        }
+
+        private string ReadResponseFromStream(BufferedStream bufferStream)
+        {
+            var sb = new StringBuilder();
+
+            int current;
+            var prev = default(char);
+            while ((current = bufferStream.ReadByte()) != -1)
+            {
+                var c = (char)current;
+                if (prev == '\r' && c == '\n') // reach at TerminateLine
+                {
+                    break;
+                }
+                else if (prev == '\r' && c == '\r')
+                {
+                    sb.Append(prev); // append prev '\r'
+                    continue;
+                }
+                else if (c == '\r')
+                {
+                    prev = c; // not append '\r'
+                    continue;
+                }
+
+                prev = c;
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
