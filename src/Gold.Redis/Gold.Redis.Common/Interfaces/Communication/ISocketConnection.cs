f4d8af7d5f826726598a6925ba7da8d41c35b79d
VainEyal <eyalvainer@gmail.com>
2019-06-18 22:41:12 +0300

    added SocketConatiner

---
+++ src/Gold.Redis/Gold.Redis.Common/Interfaces/Communication/ISocketConnection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Gold.Redis.Common.Interfaces.Communication
+{
+    public interface ISocketContainer : IDisposable
+    {
+        Socket Socket { get; }
+    }
+}
