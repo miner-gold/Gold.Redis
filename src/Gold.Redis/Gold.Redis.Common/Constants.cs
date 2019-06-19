a167785a18c2c3c87c2039df1725dce9acf37e48
VainEyal <eyalvainer@gmail.com>
2019-06-19 22:04:36 +0300

    added Request and Response constant values

---
+++ src/Gold.Redis/Gold.Redis.Common/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.Redis.Common
+{
+    public static class Constants
+    {
+        public static readonly string CrLf = "\r\n";
+    }
+
+    public static class ResponseCommandPrefixes
+    {
+        public static readonly char SimpleString = '+';
+        public static readonly char Error = '-';
+        public static readonly char Integer = ':';
+    }
+
+    public static class RequestCommandPrefixes
+    {
+        public static readonly char BulkString = '$';
+        public static readonly char Array = '*';
+    }
+}
