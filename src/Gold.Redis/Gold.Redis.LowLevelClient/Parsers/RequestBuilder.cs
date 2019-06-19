b788525d9df218ddcba56c030b8a8dd5949cca8e
VainEyal <eyalvainer@gmail.com>
2019-06-19 22:05:35 +0300

    added RequestBuilder with basic test

---
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Parsers/RequestBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gold.Redis.Common;
+using Gold.Redis.Common.Interfaces.Parsers;
+
+namespace Gold.Redis.LowLevelClient.Parsers
+{
+    public class RequestBuilder : IRequestBuilder
+    {
+
+        public string Build(string request)
+        {
+            var splicedCommand = request.Split(' ');
+            var builder = new StringBuilder();
+            builder.Append($"{RequestCommandPrefixes.Array}{splicedCommand.Length}{Constants.CrLf}");
+            foreach (var splice in splicedCommand)
+            {
+                builder.Append(
+                    $"{RequestCommandPrefixes.BulkString}{splice.Length}{Constants.CrLf}{splice}{Constants.CrLf}");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
