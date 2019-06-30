1690676cb1d63aa3a7be37f2a5789fc8b5a46f9d
VainEyal <eyalvainer@gmail.com>
2019-06-30 19:26:11 +0300

    added GetResponse parser test

--- src/Gold.Redis/Gold.Redis.Tests/ResponseParserTests.cs
+++ src/Gold.Redis/Gold.Redis.Tests/ResponseParserTests.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Gold.Redis.Common;
 using Gold.Redis.LowLevelClient.Parsers;
 using NUnit.Framework;
 
@@ -26,6 +27,21 @@ namespace Gold.Redis.Tests
             //Arrange
             var response = "+OK\r\n";
             var expectedCommand = "OK";
+
+            //Act
+            var result = await _responseParser.Parse(new StreamReader(new MemoryStream(Encoding.ASCII.GetBytes(response))));
+
+            //Assert
+            result.Should().Be(expectedCommand);
+        }
+
+        [Test]
+        public async Task ParseResponse_GetResponse_ShouldParseSuccessfully()
+        {
+            //Arrange
+            var response = $"{CommandPrefixes.BulkString}" +
+                           $"6{Constants.CrLf}RandomString{Constants.CrLf}";
+            var expectedCommand = "RandomString";
 
             //Act
             var result = await _responseParser.Parse(new StreamReader(new MemoryStream(Encoding.ASCII.GetBytes(response))));
