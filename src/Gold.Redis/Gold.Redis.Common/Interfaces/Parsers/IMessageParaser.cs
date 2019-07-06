3313a1ac5e939c55a06a83e535bb04cbc3b3b54e
Hen Shalom <xdrakolax@gmail.com>
2019-07-06 22:57:40 +0300

    add message response parser

---
+++ src/Gold.Redis/Gold.Redis.Common/Interfaces/Parsers/IMessageParaser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Gold.Redis.Common.Models;
+
+namespace Gold.Redis.Common.Interfaces.Parsers
+{
+    public interface IMessageParser : IMessageResponseParser
+    {
+        bool Test(RedisLowLevelResponse response);
+
+    }
+}
