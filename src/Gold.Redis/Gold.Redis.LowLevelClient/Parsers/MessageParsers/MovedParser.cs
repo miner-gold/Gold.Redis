a8330b999ab5c67ea072208b89a676dedca033ed
Hen Shalom <xdrakolax@gmail.com>
2019-07-06 22:58:15 +0300

    add Message response Parser and MOVED error Parser

---
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Parsers/MessageParsers/MovedParser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Gold.Redis.Common;
+using Gold.Redis.Common.Interfaces.Parsers;
+using Gold.Redis.Common.Models;
+
+namespace Gold.Redis.LowLevelClient.Parsers.MessageParsers
+{
+    public class MovedParser : IMessageParser
+    {
+        public KeyValuePair<MessageType, IDictionary<string, string>> Parse(RedisLowLevelResponse response)
+        {
+            var messageParts = response.Message.Split(' ');
+            var connectionParts = messageParts[2].Split(':');
+            var parameters = new Dictionary<string, string>
+            {
+                { "hashSlot", messageParts[1]},
+                { "host", connectionParts[0]},
+                { "port", connectionParts[1]}
+            };
+            return new KeyValuePair<MessageType, IDictionary<string, string>>(MessageType.Moved, parameters);
+        }
+
+        public bool Test(RedisLowLevelResponse response)
+        {
+            if (response.ResponseType == RedisResponseTypes.Error &&
+                response.Message.StartsWith(Constants.MovedError))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Parsers/MessageResponseParaser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Gold.Redis.Common;
+using Gold.Redis.Common.Interfaces.Parsers;
+using Gold.Redis.Common.Models;
+
+namespace Gold.Redis.LowLevelClient.Parsers
+{
+    public class MessageParser : IMessageResponseParser
+    {
+        private readonly IEnumerable<IMessageParser> _parsers;
+
+        public MessageParser(IEnumerable<IMessageParser> parsers)
+        {
+            _parsers = parsers;
+        }
+
+        public KeyValuePair<MessageType, IDictionary<string, string>> Parse(RedisLowLevelResponse response)
+        {
+            var correctParser = _parsers.First(parser => parser.Test(response));
+            return correctParser.Parse(response);
+        }
+    }
+}
