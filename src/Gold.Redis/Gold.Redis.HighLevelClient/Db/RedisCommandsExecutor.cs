e673dd3f51f1124ef6a4d3edc0775a3043dc97bf
Tomer Goldberg <tomer.gold.a@gmail.com>
2019-09-21 20:48:16 +0300

    implemented the interfaces and added implementation for the IsKeyExists function

---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisCommandsExecutor.cs
@@ -0,0 +1,35 @@
+﻿using Gold.Redis.Common.Interfaces.Communication;
+using Gold.Redis.Common.Interfaces.Db;
+using Gold.Redis.Common.Models.Commands;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Gold.Redis.HighLevelClient.Db
+{
+    public class RedisCommandsExecutor : IRedisCommandExecutor
+    {
+        private readonly IRedisConnection _redisConnection;
+        private readonly JsonSerializerSettings _serializerSettings;
+        public RedisCommandsExecutor(
+            IRedisConnection redisConnection,
+            JsonSerializerSettings serializerSettings = null)
+        {
+            _redisConnection = redisConnection;
+            _serializerSettings = serializerSettings;
+        }
+        public async Task<T> Execute<T>(Command command)
+        {
+            var commandStr = command.GetCommandString();
+            if (string.IsNullOrEmpty(commandStr))
+            {
+                throw new InvalidOperationException("Connot execute null command");
+            }
+
+            var responseStr = await _redisConnection.ExecuteCommand(commandStr);
+            return _serializerSettings == null ?
+                JsonConvert.DeserializeObject<T>(responseStr) :
+                JsonConvert.DeserializeObject<T>(responseStr, _serializerSettings);
+        }
+    }
+}
---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisGeneralOperationsDb.cs
@@ -0,0 +1,46 @@
+﻿using Gold.Redis.Common.Interfaces.Db;
+using Gold.Redis.Common.Models.Commands.Keys;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gold.Redis.HighLevelClient.Db
+{
+    public class RedisGeneralOperationsDb : IRedisDbGeneralOperations
+    {
+        private readonly IRedisCommandExecutor _commandExecutor;
+        public RedisGeneralOperationsDb(IRedisCommandExecutor commandExecutor)
+        {
+            _commandExecutor = commandExecutor;
+        }
+        public Task<T> Get<T>(string key)
+        {
+            var command = new Get
+        }
+
+        public Task<IEnumerable<string>> GetMatchingKeys(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsKeyExists(string key)
+        {
+            var command = new KeyExistsCommand
+            {
+                Key = key
+            };
+
+            return await _commandExecutor.Execute<bool>(command);
+        }
+
+        public async Task<bool> SetKeyExpire(string key, TimeSpan span)
+        {
+            var command = new ExpireKeyCommand
+            {
+                Key = key,
+                Ttl = span
+            };
+            return await _commandExecutor.Execute<bool>(command);
+        }
+    }
+}
