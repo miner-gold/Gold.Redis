24c7251f973ae0c11f767c9ac05fefdfd99698cc
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-09-24 22:45:38 +0300

    Implemented the Redis authentication request & response

---
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/RedisSocketAuthenticator.cs
@@ -0,0 +1,30 @@
+﻿using Gold.Redis.Common.Interfaces.Communication;
+using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Gold.Redis.LowLevelClient.Communication
+{
+    public class RedisSocketAuthenticator : IRedisAuthenticator
+    {
+        private readonly ISocketCommandExecutor _socketCommandExecutor;
+        public RedisSocketAuthenticator(ISocketCommandExecutor socketCommandExecutor)
+        {
+            _socketCommandExecutor = socketCommandExecutor;
+        }
+        public async Task<bool> TryAuthenticate(Socket connectionSocket, string password)
+        {
+            try
+            {
+                var authCommand = "AUTH " + password;
+                var response = await _socketCommandExecutor.ExecuteCommand(connectionSocket, authCommand);
+                return response == "OK";
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
