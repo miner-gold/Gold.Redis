91ae62d8a36e28f51c3dcf97427560d63c3a0aa9
Tomer Goldberg <tomer.gold.a@gmail.com>
2019-09-25 00:06:53 +0300

    Added file does not exits use default option

--- src/Gold.Redis/Gold.Redis.Tests/Helpers/RedisConfigurationLoader.cs
+++ src/Gold.Redis/Gold.Redis.Tests/Helpers/RedisConfigurationLoader.cs
@@ -6,9 +6,13 @@ namespace Gold.Redis.Tests.Helpers
 {
     public class RedisConfigurationLoader
     {
+        private const string FILE_PATH = "RedisConfig.json";
         public static RedisConnectionConfiguration GetConfiguration()
         {
-            var json = File.ReadAllText("RedisConfig.json");
+            if (!File.Exists(FILE_PATH))
+                return new RedisConnectionConfiguration();
+
+            var json = File.ReadAllText(FILE_PATH);
             return JsonConvert.DeserializeObject<RedisConnectionConfiguration>(json);
         }
     }
