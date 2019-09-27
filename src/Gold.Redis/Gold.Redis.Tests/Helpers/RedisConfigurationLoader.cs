295079c079c3926caa2e5bc2346b00031a1ffcd2
Tomer Goldberg <tomer.gold.a@gmail.com>
2019-09-27 12:57:55 +0300

    fixed unit tests error while merged the commits

---
+++ src/Gold.Redis/Gold.Redis.Tests/Helpers/RedisConfigurationLoader.cs
@@ -0,0 +1,15 @@
+﻿using Gold.Redis.Common.Models.Configuration;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Gold.Redis.Tests.Helpers
+{
+    public class RedisConfigurationLoader
+    {
+        public static RedisConnectionConfiguration GetConfiguration()
+        {
+            var json = File.ReadAllText("RedisConfig.json");
+            return JsonConvert.DeserializeObject<RedisConnectionConfiguration>(json);
+        }
+    }
+}
--- src/Gold.Redis/Gold.Redis.Tests/SocketTests.cs
+++ src/Gold.Redis/Gold.Redis.Tests/SocketTests.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
@@ -79,3 +80,94 @@ namespace Gold.Redis.Tests
     }
 }
 
+=======
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Gold.Redis.Common.Interfaces.Communication;
+using Gold.Redis.LowLevelClient.Communication;
+using Gold.Redis.Tests.Helpers;
+using Moq;
+using NUnit.Framework;
+
+namespace Gold.Redis.Tests
+{
+    [TestFixture]
+    public class SocketTests
+    {
+        private Mock<IRedisAuthenticator> _authenticator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _authenticator = new Mock<IRedisAuthenticator>();
+        }
+
+        [Test]
+        public void CreateSocketContainer_ShouldFreeSocketWhenFinished()
+        {
+            // Arrange
+            var socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            var connectionsContainerMock = new Mock<IConnectionsContainer>();
+            var socketContainer = new SocketContainer(socket,
+                connectionsContainerMock.Object);
+
+            // Act
+            using (socketContainer)
+            {
+
+            }
+
+            //Assert
+            connectionsContainerMock.Verify(container => container.FreeSocket(socket), Times.Once);
+        }
+
+        [Test]
+        public async Task CreateSocket_FreeTheSocket_VerifyThatTheSameSocketReturned()
+        {
+            // Arrange
+            var configuration = RedisConfigurationLoader.GetConfiguration();
+            configuration.MaxConnections = 1;
+            _authenticator.Setup(auth => auth.TryAuthenticate(It.IsAny<Socket>(), It.IsAny<string>())).ReturnsAsync(true);
+            var connectionContainer = new SocketsConnectionsContainer(configuration, _authenticator.Object);
+
+            // Act
+            var socketContainer = await connectionContainer.GetSocket();
+            var socket = socketContainer.Socket;
+            socketContainer.Dispose();
+            var secondContainer = await connectionContainer.GetSocket();
+
+            // Assert
+            secondContainer.Socket.Should().BeSameAs(socket);
+        }
+
+        [Test]
+        public async Task CreateSocket_WaitUntilSocketIsFree_VerifyThatTheSameSocketReturned()
+        {
+
+            // Arrange
+            var configuration = RedisConfigurationLoader.GetConfiguration();
+            configuration.MaxConnections = 1;
+            _authenticator.Setup(auth => auth.TryAuthenticate(It.IsAny<Socket>(), It.IsAny<string>())).ReturnsAsync(true);
+            var connectionContainer = new SocketsConnectionsContainer(configuration, _authenticator.Object);
+
+            // Act
+            var socketContainer = await connectionContainer.GetSocket();
+            var socket = socketContainer.Socket;
+            var disposeTask = Task.Run(async () =>
+            {
+                await Task.Delay(200);
+                socketContainer.Dispose();
+            });
+            var secondContainer = await connectionContainer.GetSocket();
+
+            // Assert
+            disposeTask.IsCompleted.Should().BeTrue();
+            secondContainer.Socket.Should().BeSameAs(socket);
+        }
+    }
+}
+
+>>>>>>> 48e617b... Fixed existing U.T to handle passwords
