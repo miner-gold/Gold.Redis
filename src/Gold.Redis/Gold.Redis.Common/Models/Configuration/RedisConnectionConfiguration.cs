addd506a1e20e13a14b7314183427ac5bd22343d
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-09-27 12:48:13 +0300

    Added default options for the redis configurations

--- src/Gold.Redis/Gold.Redis.Common/Models/Configuration/RedisConnectionConfiguration.cs
+++ src/Gold.Redis/Gold.Redis.Common/Models/Configuration/RedisConnectionConfiguration.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +6,9 @@ namespace Gold.Redis.Common.Models.Configuration
 {
     public class RedisConnectionConfiguration
     {
-        public string Host { get; set; }
+        public string Host { get; set; } = "localhost";
         public int Port { get; set; } = 6379;
         public string Password { get; set; }
+        public int MaxConnections { get; set; } = 1;
     }
 }
