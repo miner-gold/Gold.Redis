265ef0ef4cf24b80ea8f52b4daf39ebd05a6b266
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-09-27 12:44:37 +0300

    Changed sokect commands sending
    Seperated to two interfaces -> one with socket handling and one with socket selection

---
+++ src/Gold.Redis/Gold.Redis.Common/Interfaces/Communication/IRedisCommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Gold.Redis.Common.Interfaces.Communication
+{
+    public interface IRedisCommandHandler
+    {
+        Task<string> ExecuteCommand(string command);
+    }
+}
