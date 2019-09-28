d46ce2f6252679cebac6d59bbf714833e0635a99
VainEyal <eyalvainer@gmail.com>
2019-09-28 22:59:17 +0300

    added all basic responses

---
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Responses/Response.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Gold.Redis.LowLevelClient.Responses
+{
+    public abstract class Response
+    {
+    }
+
+    public class SimpleStringResponse : Response
+    {
+        public string Response { get; set; }
+    }
+
+    public class ErrorResponse : Response
+    {
+        public string ErrorMessage { get; set; }
+    }
+
+    public class IntResponse : Response
+    {
+        public int Response { get; set; }
+    }
+
+    public class BulkStringResponse : Response //TODO: consider changing to inherit from SimpleStringResponse
+    {
+        public int StringLength { get; set; }
+        public string Response { get; set; }
+    }
+
+    public class ArrayResponse : Response
+    {
+        public IEnumerable<Response> Responses { get; set; }
+    }
+}
