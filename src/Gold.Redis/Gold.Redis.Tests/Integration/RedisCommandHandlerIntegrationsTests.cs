8a92ea47c70e0ce906511b2a90646d00569e1a5d
VainEyal <eyalvainer@gmail.com>
2019-09-29 23:49:09 +0300

    added fluentassetions

--- src/Gold.Redis/Gold.Redis.Tests/Integration/RedisCommandHandlerIntegrationsTests.cs
+++ src/Gold.Redis/Gold.Redis.Tests/Integration/RedisCommandHandlerIntegrationsTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Authentication;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Gold.Redis.Common;
 using Gold.Redis.Common.Configuration;
 using Gold.Redis.LowLevelClient.Interfaces.Parsers;
