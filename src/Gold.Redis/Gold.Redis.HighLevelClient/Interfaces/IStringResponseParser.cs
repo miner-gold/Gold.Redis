fad2320c20d6c74df6cd880db9b676e938437e92
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-10-01 13:08:22 +0300

    moved json response parsing to its own class with interface
    For better DI and SOLID handling

---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Interfaces/IStringResponseParser.cs
@@ -0,0 +1,13 @@
+﻿using Gold.Redis.LowLevelClient.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.Redis.HighLevelClient.Interfaces
+{
+    public interface IStringResponseParser
+    {
+        T Parse<T>(string response);
+        string Stringify<T>(T item);
+    }
+}
---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/ResponseParsers/JsonResponseParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Gold.Redis.HighLevelClient.Interfaces;
+using Gold.Redis.LowLevelClient.Interfaces.Parsers;
+using Gold.Redis.LowLevelClient.Responses;
+using Newtonsoft.Json;
+
+namespace Gold.Redis.HighLevelClient.ResponseParsers
+{
+    public class JsonResponseParser : IStringResponseParser
+    {
+        private readonly JsonSerializer _serializer;
+        public JsonResponseParser(JsonSerializerSettings settings = null)
+        {
+            if (settings != null)
+            {
+                _serializer = JsonSerializer.Create(settings);
+            }
+            else
+            {
+                _serializer = JsonSerializer.CreateDefault();
+            }
+        }
+        public T Parse<T>(string response)
+        {
+            if (response == null)
+                return default(T);
+
+            using (var stringReader = new StringReader(response))
+            using (var jsonReader = new JsonTextReader(stringReader))
+            {
+                return _serializer.Deserialize<T>(jsonReader);
+            }
+        }
+
+        public string Stringify<T>(T item)
+        {
+            if (item == null)
+                return null;
+            if (item is string itemStr)
+                return itemStr;
+
+            StringBuilder sb = new StringBuilder();
+            using (var sr = new StringWriter(sb))
+            using (var jsonTextWriter = new JsonTextWriter(sr))
+            {
+                _serializer.Serialize(jsonTextWriter, item, item.GetType());
+                return sb.ToString();
+            }
+        }
+    }
+}
