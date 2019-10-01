0ab71f3875bb65ffe47a29e728836766b429e2ff
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-10-01 13:09:53 +0300

    Added the new functionalities to the General db
    TODO: think about better location for the diffrent functions types

--- src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisGeneralOperationsDb.cs
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisGeneralOperationsDb.cs
@@ -1,31 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Gold.Redis.Common;
+using Gold.Redis.HighLevelClient.Commands.General;
 using Gold.Redis.HighLevelClient.Commands.Keys;
+using Gold.Redis.HighLevelClient.Commands.Search;
 using Gold.Redis.HighLevelClient.Interfaces;
+using Gold.Redis.HighLevelClient.Utils;
+using Gold.Redis.LowLevelClient.Responses;
 
 namespace Gold.Redis.HighLevelClient.Db
 {
     public class RedisGeneralOperationsDb : IRedisDbGeneralOperations
     {
         private readonly IRedisCommandExecutor _commandExecutor;
-        public RedisGeneralOperationsDb(IRedisCommandExecutor commandExecutor)
+        private readonly IStringResponseParser _stringResponseParser;
+        public RedisGeneralOperationsDb(IRedisCommandExecutor commandExecutor, IStringResponseParser stringResponseParser)
         {
             _commandExecutor = commandExecutor;
+            _stringResponseParser = stringResponseParser;
         }
+        /// <summary>
+        /// Get the value of a key,
+        /// If the key does not exists in the db, the default value of T will be returned
+        /// </summary>
+        /// <typeparam name="T">The expected Type of the returned value</typeparam>
+        /// <param name="key">The redis key</param>
+        /// <returns></returns>
         public async Task<T> Get<T>(string key)
         {
-            var command = new GetCommand
+            var command = new GetKeyValueCommand
             {
                 Key = key
             };
 
-            return await _commandExecutor.Execute<T>(command);
+            var response = await _commandExecutor.Execute<BulkStringResponse>(command);
+            return _stringResponseParser.Parse<T>(response?.Response);
         }
 
-        public Task<IEnumerable<string>> GetMatchingKeys(string pattern)
+        public async Task<IEnumerable<string>> GetMatchingKeys(string pattern = "*")
         {
-            throw new NotImplementedException();
+            var command = new KeysCommand
+            {
+                Pattern = pattern
+            };
+            var response = await _commandExecutor.Execute<ArrayResponse>(command);
+
+            return response?.Responses
+                .OfType<BulkStringResponse>()
+                .Select(item => item.Response);
+        }
+
+        public async Task<bool> DeleteKey(string key)
+        {
+            var command = new DeleteKeyCommand
+            {
+                Key = key
+            };
+            var response = await _commandExecutor.Execute<IntResponse>(command);
+
+            return response.Response == 1;
+        }
+
+        public async Task<bool> FlushDb(bool isAsync = false)
+        {
+            var command = new FlushDbCommand
+            {
+                IsAsync = isAsync
+            };
+            var response = await _commandExecutor.Execute<SimpleStringResponse>(command);
+            return response?.Response == Constants.OkResponse;
+        }
+
+        public async Task<bool> SetKey<T>(string key, T value, TimeSpan? expirySpan = null, KeyAssertion assertion = KeyAssertion.Any)
+        {
+            var command = new SetKeyCommand
+            {
+                Key = key,
+                Value = _stringResponseParser.Stringify(value),
+                Assertion = assertion,
+                ExpirySpan = expirySpan
+            };
+            var response = await _commandExecutor.Execute<SimpleStringResponse>(command);
+            return response?.Response == Constants.OkResponse;
         }
 
         public async Task<bool> IsKeyExists(string key)
@@ -35,7 +93,8 @@ namespace Gold.Redis.HighLevelClient.Db
                 Key = key
             };
 
-            return await _commandExecutor.Execute<bool>(command);
+            var response = await _commandExecutor.Execute<IntResponse>(command);
+            return response?.Response == 1;
         }
 
         public async Task<bool> SetKeyExpire(string key, TimeSpan span)
@@ -45,7 +104,8 @@ namespace Gold.Redis.HighLevelClient.Db
                 Key = key,
                 Ttl = span
             };
-            return await _commandExecutor.Execute<bool>(command);
+            var response = await _commandExecutor.Execute<IntResponse>(command);
+            return response?.Response == 1;
         }
     }
 }
--- src/Gold.Redis/Gold.Redis.HighLevelClient/Interfaces/IRedisDbGeneralOperations.cs
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Interfaces/IRedisDbGeneralOperations.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Gold.Redis.HighLevelClient.Utils;
 
 namespace Gold.Redis.HighLevelClient.Interfaces
 {
     public interface IRedisDbGeneralOperations
     {
+        Task<bool> SetKey<T>(string key, T value, TimeSpan? expirySpan = null, KeyAssertion assertion = KeyAssertion.Any);
         Task<bool> IsKeyExists(string key);
         Task<T> Get<T>(string key);
         Task<bool> SetKeyExpire(string key, TimeSpan span);
-        Task<IEnumerable<string>> GetMatchingKeys(string pattern);
+        Task<IEnumerable<string>> GetMatchingKeys(string pattern = "*");
+        Task<bool> DeleteKey(string key);
 
+        /// <summary>
+        /// Removes all the items from the db
+        /// </summary>
+        /// <param name="isAsync">Indicate if the operation should be preformed in async manner (require redis 4.0.0 or higher)</param>
+        /// <returns></returns>
+        Task<bool> FlushDb(bool isAsync = false);
     }
 }
