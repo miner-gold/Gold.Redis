1b6ea13cb6079990385d2201976733513e7c372e
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-10-01 13:08:57 +0300

    Added the KeyAssertion for the SET command and future transaction commands

---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Utils/KeyAssertion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.Redis.HighLevelClient.Utils
+{
+    public enum KeyAssertion
+    {
+        Any,
+        MustExist,
+        MustNotExists
+    }
+}
