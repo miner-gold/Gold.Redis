4d1653c3f4ea211c23ce164a7a773c15ff37c998
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-10-21 00:00:52 +0300

    Created Redis Scan command execution Implementaion
    A helper class that preform full scan using the cursor logic

---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisScanner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gold.Redis.HighLevelClient.Interfaces;
+using Gold.Redis.HighLevelClient.Models.Commands.Search;
+using Gold.Redis.LowLevelClient.Responses;
+
+namespace Gold.Redis.HighLevelClient.Db
+{
+    public class RedisScanner : IRedisScanner
+    {
+        private readonly IRedisCommandExecutor _commandExecutor;
+
+        public RedisScanner(IRedisCommandExecutor commandExecutor)
+        {
+            _commandExecutor = commandExecutor;
+        }
+
+        public async Task<HashSet<string>> ExecuteFullScan(ScanCommandBase command)
+        {
+            var result = new HashSet<string>();
+            int cursor = 0;
+            do
+            {
+                command.Cursor = cursor;
+                var response = await _commandExecutor.Execute<ArrayResponse>(command);
+                if (response?.Responses == null)
+                    return null;
+
+                var responsesList = response.Responses.ToList();
+                if (responsesList.Count < 2)
+                    return null;
+
+                cursor = (responsesList[0] as IntResponse).Response;
+                foreach (var innerResponse in (responsesList[1] as ArrayResponse).Responses)
+                {
+                    result.Add((innerResponse as SimpleStringResponse).Response);
+                }
+            }
+            while (cursor != 0);
+
+            return result;
+        }
+    }
+}
---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Interfaces/IRedisScanner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Gold.Redis.HighLevelClient.Models.Commands.Search;
+
+namespace Gold.Redis.HighLevelClient.Interfaces
+{
+    public interface IRedisScanner
+    {
+        Task<HashSet<string>> ExecuteFullScan(ScanCommandBase command);
+    }
+}
