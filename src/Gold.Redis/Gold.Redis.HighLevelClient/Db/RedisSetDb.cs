f2e54650334944406efc614b7fcee5c7b33b3dfe
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-10-21 00:06:48 +0300

    Created the redis SET datatype operations implementaions
    DB + INTERFACE

---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisSetDb.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gold.Redis.HighLevelClient.Interfaces;
+using Gold.Redis.HighLevelClient.Models.Commands.Search;
+using Gold.Redis.HighLevelClient.Models.Commands.Set;
+using Gold.Redis.LowLevelClient.Parsers.PrefixParsers;
+using Gold.Redis.LowLevelClient.Responses;
+
+namespace Gold.Redis.HighLevelClient.Db
+{
+    public class RedisSetDb : IRedisSetDb
+    {
+        private readonly IRedisScanner _scanner;
+        private readonly IRedisCommandExecutorHelper _commandExecutor;
+        private readonly IStringResponseParser _parser;
+
+        public RedisSetDb(
+            IRedisCommandExecutorHelper commandExecutor,
+            IStringResponseParser parser,
+            IRedisScanner scanner)
+        {
+            _commandExecutor = commandExecutor;
+            _parser = parser;
+            _scanner = scanner;
+        }
+
+        public async Task<bool> SetAdd<T>(string key, T item) => await SetAdd<T>(key, new List<T> { item });
+
+        public async Task<bool> SetAdd<T>(string key, IEnumerable<T> items)
+        {
+            var itemsStr = items.Select(item => _parser.Stringify(item)).Distinct().ToList();
+            var command = new SetAddCommand
+            {
+                SetKey = key,
+                Items = itemsStr
+            };
+
+            var response = await _commandExecutor.ExecuteIntResponseCommand(command);
+            return response == itemsStr.Count;
+        }
+
+        public async Task<int> SetCount(string key)
+        {
+            var command = new SetCardinalityCommand
+            {
+                SetKey = key
+            };
+            return await _commandExecutor.ExecuteIntResponseCommand(command);
+        }
+
+        public async Task<IEnumerable<T>> SetDiff<T>(string firstKey, params string[] keys)
+        {
+            var command = new SetDifferenceCommand
+            {
+                FirstSetKey = firstKey,
+                DifferentSetKeys = keys
+            };
+            return await _commandExecutor.ExecuteArrayResponseCommand<T>(command);
+        }
+
+        public async Task<int> SetDiffStore(string resultingSetKey, string firstKey, params string[] keys)
+        {
+            var command = new SetDifferenceAndStoreCommand
+            {
+                NewSetKey = resultingSetKey,
+                DifferentSetKeys = keys,
+                FirstSetKey = firstKey
+            };
+            return await _commandExecutor.ExecuteIntResponseCommand(command);
+        }
+
+        public async Task<IEnumerable<T>> SetIntersect<T>(params string[] keys)
+        {
+            var command = new SetIntersectCommand
+            {
+                SetsKeys = keys
+            };
+            return await _commandExecutor.ExecuteArrayResponseCommand<T>(command);
+        }
+
+        public async Task<int> SetIntersectStore(string resultingSetKey, params string[] keys)
+        {
+            var command = new SetIntersectAndStoreCommand()
+            {
+                NewSetKey = resultingSetKey,
+                SetsKeys = keys
+            };
+            return await _commandExecutor.ExecuteIntResponseCommand(command);
+        }
+
+        public async Task<bool> SetIsExists<T>(string key, T item)
+        {
+            var command = new SetIsMemberExistsCommand
+            {
+                SetKey = key,
+                Member = _parser.Stringify(item)
+            };
+            return await _commandExecutor.ExecuteBooleanIntResponseCommand(command);
+        }
+
+        public async Task<IEnumerable<T>> GetSetMembers<T>(string key)
+        {
+            var command = new SetMembersCommand
+            {
+                SetKey = key
+            };
+            return await _commandExecutor.ExecuteArrayResponseCommand<T>(command);
+        }
+
+        public async Task<bool> MoveItemBetweenSets<T>(string originSet, string destinationSet, T item)
+        {
+            var command = new SetMoveCommand
+            {
+                SourceSet = originSet,
+                DestinationSet = destinationSet,
+                Item = _parser.Stringify(item)
+            };
+            return await _commandExecutor.ExecuteBooleanIntResponseCommand(command);
+        }
+
+        public async Task<IEnumerable<T>> PopItemsFromSet<T>(string key, int numberOfItems)
+        {
+            var command = new SetPopCommand
+            {
+                SetKey = key,
+                NumberOfElements = numberOfItems
+            };
+            return await _commandExecutor.ExecuteArrayResponseCommand<T>(command);
+        }
+
+        public async Task<T> PopItemFromSet<T>(string key) => (await PopItemsFromSet<T>(key, 1)).FirstOrDefault();
+
+        public Task<IEnumerable<T>> GetRandomItemsFromSet<T>(string key, int count, bool allowMultiple = false)
+        {
+            var command = new SetGetRandomMembersCommand
+            {
+                SetKey = key,
+                NumberOfItems = count,
+                AllowMultipleSameItemsReturn = allowMultiple
+            };
+            return _commandExecutor.ExecuteArrayResponseCommand<T>(command);
+        }
+
+        public async Task<T> GetRandomItemFromSet<T>(string key) =>
+            (await GetRandomItemsFromSet<T>(key, 1)).FirstOrDefault();
+
+        public async Task<bool> SetRemove<T>(string key, params T[] items)
+        {
+            var command = new SetRemoveCommand
+            {
+                SetKey = key,
+                ItemsToRemove = items.Select(item => _parser.Stringify(item)).ToArray()
+            };
+            return await _commandExecutor.ExecuteIntResponseCommand(command) == items.Length;
+        }
+
+        public async Task<IEnumerable<T>> UnionSets<T>(params string[] keys)
+        {
+            var command = new SetUnionCommand
+            {
+                SetKeys = keys
+            };
+            return await _commandExecutor.ExecuteArrayResponseCommand<T>(command);
+        }
+
+        public async Task<int> UnionSetsStore(string destinationKey, params string[] keys)
+        {
+            var command = new SetUnionAndStoreCommand
+            {
+                NewSetKey = destinationKey,
+                SetKeys = keys
+            };
+            return await _commandExecutor.ExecuteIntResponseCommand(command);
+        }
+
+        public async Task<IEnumerable<T>> SetScan<T>(string key, string pattern = null, int? countHint = null)
+        {
+            var command = new SetScanCommand
+            {
+                SetKey = key,
+                CountHint = countHint,
+                Match = pattern
+            };
+            var response = await _scanner.ExecuteFullScan(command);
+            return response.Select(item => _parser.Parse<T>(item));
+        }
+    }
+}
---
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Interfaces/IRedisSetDb.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gold.Redis.HighLevelClient.Interfaces
+{
+    public interface IRedisSetDb
+    {
+
+        /// <summary>
+        /// Same as SetAdd but with single item
+        /// <param name="key">the identifier of the SET</param>
+        /// </summary>
+        Task<bool> SetAdd<T>(string key, T item);
+
+        /// <summary>
+        /// Add the specified members to the set stored at key.
+        /// Specified members that are already a member of this set are ignored.
+        /// If key does not exist, a new set is created before adding the specified members
+        /// <param name="key">the identifier of the SET</param>
+        /// </summary>
+        Task<bool> SetAdd<T>(string key, IEnumerable<T> items);
+
+        /// <summary>
+        /// Returns the number of elements of the set stored at key
+        /// </summary>
+        /// <param name="key">the identifier of the SET</param>
+        /// <returns>Number of the set elements, 0 if the set does not exists</returns>
+        Task<int> SetCount(string key);
+
+        /// <summary>
+        /// Returns the members of the set resulting from the difference between the first set and all the successive sets
+        /// </summary>
+        /// <typeparam name="T">The data type of the item in the set</typeparam>
+        /// <param name="firstKey"></param>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> SetDiff<T>(string firstKey, params string[] keys);
+
+        /// <summary>
+        /// This command is equal to SetDiff, but instead of returning the resulting set, it is stored in destination
+        /// </summary>
+        /// <param name="resultingSetKey">The new set with the DIFF operation result</param>
+        /// <param name="firstKey"></param>
+        /// <param name="keys"></param>
+        /// <returns>The number of items that are in the result set</returns>
+        Task<int> SetDiffStore(string resultingSetKey, string firstKey, params string[] keys);
+
+        /// <summary>
+        /// Returns the members of the set resulting from the intersection of all the given sets.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> SetIntersect<T>(params string[] keys);
+
+        /// <summary>
+        /// This command is equal to SetIntersect, but instead of returning the resulting set, it is stored in destination
+        /// </summary>
+        /// <param name="resultingSetKey">The new set with the DIFF operation result</param>
+        /// <param name="keys"></param>
+        /// <returns>The number of items that are in the result set</returns>
+        Task<int> SetIntersectStore(string resultingSetKey, params string[] keys);
+
+        /// <summary>
+        /// Returns if the given item is in the set
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        Task<bool> SetIsExists<T>(string key, T item);
+
+        /// <summary>
+        /// Gets all the items of a set from a given key
+        /// </summary>
+        /// <typeparam name="T">The data type of an item in the set</typeparam>
+        /// <param name="key">The SET item key</param>
+        /// <returns>A list of the items </returns>
+        Task<IEnumerable<T>> GetSetMembers<T>(string key);
+
+        /// <summary>
+        /// Move member from the set at source to the set at destination.
+        /// This operation is atomic. In every given moment the element will appear to be a member of source or destination for other clients
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="originSet">The key of the set we want to take the item from</param>
+        /// <param name="destinationSet">The key of the set we want to put the item in</param>
+        /// <param name="item">The item we want to move between sets</param>
+        /// <returns>True if the operation was successful, False otherwise</returns>
+        Task<bool> MoveItemBetweenSets<T>(string originSet, string destinationSet, T item);
+
+        /// <summary>
+        /// Removes and returns number of random elements from the set value store at key
+        /// </summary>
+        /// <typeparam name="T">The set items data type</typeparam>
+        /// <param name="key">The key of the set</param>
+        /// <param name="numberOfItems">The number of items we want to take from the set</param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> PopItemsFromSet<T>(string key, int numberOfItems);
+
+        /// <summary>
+        /// Same as PopItemsFromSet, but only pop one random item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        Task<T> PopItemFromSet<T>(string key);
+
+        /// <summary>
+        /// Get number of random items from the SET
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">the set key</param>
+        /// <param name="count">the number of requested items to be returned</param>
+        /// <param name="allowMultiple">If false, the command will only return distinct items. otherwise the command will return items even if it exits multiple times</param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> GetRandomItemsFromSet<T>(string key, int count, bool allowMultiple = false);
+
+        /// <summary>
+        /// Return single random item from the set
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        Task<T> GetRandomItemFromSet<T>(string key);
+
+        /// <summary>
+        /// Removes an item from the set
+        /// </summary>
+        /// <typeparam name="T">The data type of an item in the set</typeparam>
+        /// <param name="key">The SET item key</param>
+        /// <param name="item"></param>
+        /// <returns>True if the item was removed, false if the item does not exists nor the set does not exists</returns>
+        Task<bool> SetRemove<T>(string key, params T[] itemsToRemove);
+
+        /// <summary>
+        /// Returns the members of the set resulting from the union of all the given sets
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        Task<IEnumerable<T>> UnionSets<T>(params string[] keys);
+
+        /// <summary>
+        /// This command is equal to UnionSets, but instead of returning the resulting set, it is stored in destination
+        /// </summary>
+        /// <param name="destinationKey">The resulting SET key</param>
+        /// <param name="keys"></param>
+        /// <returns>The number of items that were added to the new set</returns>
+        Task<int> UnionSetsStore(string destinationKey, params string[] keys);
+
+        /// <summary>
+        /// See https://redis.io/commands/scan for more information about the scan command
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="pattern"></param>
+        /// <param name="countHint"></param>
+        /// <returns>All of the item that match the given pattern in the set</returns>
+        Task<IEnumerable<T>> SetScan<T>(string key, string pattern = null, int? countHint = null);
+
+    }
+}
