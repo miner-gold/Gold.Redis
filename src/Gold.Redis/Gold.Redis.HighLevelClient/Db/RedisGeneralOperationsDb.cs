e91e6c99cfc69631369f953718e9c52aa1e58285
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-10-25 11:49:43 +0300

    Added redis db support for simpler client experience

--- src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisGeneralOperationsDb.cs
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisGeneralOperationsDb.cs
@@ -12,31 +12,12 @@ using Gold.Redis.LowLevelClient.Responses;
 
 namespace Gold.Redis.HighLevelClient.Db
 {
-    public class RedisGeneralOperationsDb : IRedisDbGeneralOperations
+    public class RedisGeneralOperationsDb : IRedisGeneralOperationsDb
     {
         private readonly IRedisCommandExecutor _commandExecutor;
-        private readonly IStringResponseParser _stringResponseParser;
-        public RedisGeneralOperationsDb(IRedisCommandExecutor commandExecutor, IStringResponseParser stringResponseParser)
+        public RedisGeneralOperationsDb(IRedisCommandExecutor commandExecutor)
         {
             _commandExecutor = commandExecutor;
-            _stringResponseParser = stringResponseParser;
-        }
-        /// <summary>
-        /// Get the value of a key,
-        /// If the key does not exists in the db, the default value of T will be returned
-        /// </summary>
-        /// <typeparam name="T">The expected Type of the returned value</typeparam>
-        /// <param name="key">The redis key</param>
-        /// <returns></returns>
-        public async Task<T> Get<T>(string key)
-        {
-            var command = new GetKeyValueCommand
-            {
-                Key = key
-            };
-
-            var response = await _commandExecutor.Execute<BulkStringResponse>(command);
-            return _stringResponseParser.Parse<T>(response?.Response);
         }
 
         public async Task<IEnumerable<string>> GetMatchingKeys(string pattern = "*")
@@ -68,19 +49,6 @@ namespace Gold.Redis.HighLevelClient.Db
             var command = new FlushDbCommand
             {
                 IsAsync = isAsync
-            };
-            var response = await _commandExecutor.Execute<SimpleStringResponse>(command);
-            return response?.Response == Constants.OkResponse;
-        }
-
-        public async Task<bool> SetKey<T>(string key, T value, TimeSpan? expirySpan = null, KeyAssertion assertion = KeyAssertion.Any)
-        {
-            var command = new SetKeyCommand
-            {
-                Key = key,
-                Value = _stringResponseParser.Stringify(value),
-                Assertion = assertion,
-                ExpirySpan = expirySpan
             };
             var response = await _commandExecutor.Execute<SimpleStringResponse>(command);
             return response?.Response == Constants.OkResponse;
--- src/Gold.Redis/Gold.Redis.HighLevelClient/Interfaces/IRedisKeysDb.cs
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Interfaces/IRedisKeysDb.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Gold.Redis.HighLevelClient.Models.Utils;
 
 namespace Gold.Redis.HighLevelClient.Interfaces
 {
-    public interface IRedisKeysDb : IRedisDbGeneralOperations
+    public interface IRedisKeysDb
     {
-        Task<bool> Set<T>(string key, T value);
+        Task<bool> SetKey<T>(string key, T value, TimeSpan? expirySpan = null, KeyAssertion assertion = KeyAssertion.Any);
+        Task<T> Get<T>(string key);
     }
 }
