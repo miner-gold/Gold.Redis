c84befea8007bf373cd7de2d3631f3231a0df8aa
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-11-19 22:37:15 +0200

    Renamed the redis command exector to single command exector

--- src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisSingleCommandExecutor.cs
+++ src/Gold.Redis/Gold.Redis.HighLevelClient/Db/RedisSingleCommandExecutor.cs
@@ -1,21 +1,23 @@
 ﻿using Gold.Redis.LowLevelClient.Interfaces;
 using System;
 using System.Threading.Tasks;
+using Gold.Redis.Common.Interfaces.Pipelining;
 using Gold.Redis.HighLevelClient.Interfaces;
 using Gold.Redis.HighLevelClient.Models.Commands;
 using Gold.Redis.LowLevelClient.Responses;
 
 namespace Gold.Redis.HighLevelClient.Db
 {
-    public class RedisCommandsExecutor : IRedisCommandExecutor
+    public class RedisSingleCommandExecutor : IRedisCommandExecutor
     {
         private readonly IRedisCommandHandler _redisCommandHandler;
-        public RedisCommandsExecutor(
+
+        public RedisSingleCommandExecutor(
             IRedisCommandHandler redisConnection)
         {
             _redisCommandHandler = redisConnection;
         }
-        public async Task<T> Execute<T>(Command command) where T: Response
+        public async Task<T> Execute<T>(Command command) where T : Response
         {
             var commandStr = command.GetCommandString();
             if (string.IsNullOrEmpty(commandStr))
