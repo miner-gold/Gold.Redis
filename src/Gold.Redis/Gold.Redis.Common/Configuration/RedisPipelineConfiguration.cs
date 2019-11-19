551441d2701713c78501dc4fb53355dc467df3e2
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-11-19 22:52:19 +0200

    Lowered the default max wait time to one second

--- src/Gold.Redis/Gold.Redis.Common/Configuration/RedisPipelineConfiguration.cs
+++ src/Gold.Redis/Gold.Redis.Common/Configuration/RedisPipelineConfiguration.cs
@@ -6,6 +6,6 @@ namespace Gold.Redis.Common.Configuration
     {
         public int MaxItemsPerRequest { get; set; } = 1000;
         public int MaxDegreeOfParallelism { get; set; } = 1;
-        public TimeSpan MaxWaitTime { get; set; } = TimeSpan.FromSeconds(5);
+        public TimeSpan MaxWaitTime { get; set; } = TimeSpan.FromSeconds(1);
     }
 }
