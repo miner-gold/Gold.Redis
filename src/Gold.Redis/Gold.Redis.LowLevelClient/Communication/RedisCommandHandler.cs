1073752ed486133d69eab3b8d80304fec6d45c5e
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-11-19 22:35:19 +0200

    Added the implmentation for multiple cpmmands handling

--- src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/RedisCommandHandler.cs
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/RedisCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Gold.Redis.LowLevelClient.Interfaces;
 using Gold.Redis.LowLevelClient.Responses;
 
@@ -14,13 +16,24 @@ namespace Gold.Redis.LowLevelClient.Communication
             _socketCommandExecutor = socketCommand;
         }
 
-        public async Task<T> ExecuteCommand<T>(string command)
+        public async Task<IEnumerable<T>> ExecuteCommands<T>(IEnumerable<string> commands)
             where T : Response
         {
             using (var socketContainer = await _connections.GetSocket())
             {
-                return await _socketCommandExecutor.ExecuteCommand<T>(socketContainer.Socket, command);
+                return await _socketCommandExecutor.ExecuteCommands<T>(socketContainer.Socket, commands.ToArray());
             }
         }
+
+        public async Task<T> ExecuteCommand<T>(string command) where T : Response
+        {
+            using (var socketContainer = await _connections.GetSocket())
+            {
+                return await _socketCommandExecutor.ExecuteCommand<T>(
+                    socketContainer.Socket,
+                    command);
+            }
+        }
+        
     }
 }
--- src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/SocketCommandExecutor.cs
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/SocketCommandExecutor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,12 +23,40 @@ namespace Gold.Redis.LowLevelClient.Communication
             _requestBuilder = builder;
             _responseParser = parser;
         }
-        public async Task<T> ExecuteCommand<T>(Socket socket, string command)
+
+        public async Task<IEnumerable<T>> ExecuteCommands<T>(Socket socket, params string[] commands)
             where T : Response
         {
-            var bytes = Encoding.ASCII.GetBytes(_requestBuilder.Build(command));
-            var bytesAsArraySegment = new ArraySegment<byte>(bytes);
-            await socket.SendAsync(bytesAsArraySegment, SocketFlags.None);
+            var responses = new T[commands.Length];
+            var arraySegment = GetCommandArraySegment<T>(commands);
+            await socket.SendAsync(arraySegment, SocketFlags.None);
+
+            using (var networkStream = new NetworkStream(socket))
+            using (var streamReader = new StreamReader(networkStream))
+            {
+                for (int i = 0; i < commands.Length; i++)
+                {
+                    var response = await _responseParser.Parse(streamReader);
+
+                    if (response == null)
+                        return null;
+
+                    if (response is ErrorResponse errorResponse)
+                    {
+                        throw new Exception(errorResponse.ErrorMessage);
+                    }
+
+                    responses[i] = (T)response;
+                }
+            }
+
+            return responses;
+        }
+
+        public async Task<T> ExecuteCommand<T>(Socket socket, string command) where T : Response
+        {
+            var arraySegment = GetCommandArraySegment<T>(command);
+            await socket.SendAsync(arraySegment, SocketFlags.None);
 
             using (var networkStream = new NetworkStream(socket))
             using (var streamReader = new StreamReader(networkStream))
@@ -49,6 +79,12 @@ namespace Gold.Redis.LowLevelClient.Communication
                 throw new InvalidCastException($"Could not cast response to the desired type." +
                                                $" Expected type: ${typeof(T)} Actual type: ${response?.GetType()}");
             }
+        }
+
+        private ArraySegment<byte> GetCommandArraySegment<T>(params string[] commands) where T : Response
+        {
+            var bytes = Encoding.ASCII.GetBytes(_requestBuilder.Build(commands));
+            return new ArraySegment<byte>(bytes);
         }
     }
 }
