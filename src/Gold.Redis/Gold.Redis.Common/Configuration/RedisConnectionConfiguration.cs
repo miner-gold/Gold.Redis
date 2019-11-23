68ac5c19f49d3da8c55c4e79b17f9e58b85afb67
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-11-23 22:54:02 +0200

    Added timeout options to the configuration file

--- src/Gold.Redis/Gold.Redis.Common/Configuration/RedisConnectionConfiguration.cs
+++ src/Gold.Redis/Gold.Redis.Common/Configuration/RedisConnectionConfiguration.cs
@@ -1,11 +1,20 @@
-﻿namespace Gold.Redis.Common.Configuration
+﻿using System;
+
+namespace Gold.Redis.Common.Configuration
 {
     public class RedisConnectionConfiguration
     {
+        //Server Info
         public string Host { get; set; } = "localhost";
         public int Port { get; set; } = 6379;
         public string Password { get; set; }
         public int MaxConnections { get; set; } = 1;
+
+        //Connection and Retries info
+        public int ConnectionRetries = 3;
+        public TimeSpan ConnectionFailedWaitTime = TimeSpan.FromSeconds(3);
+        public TimeSpan RequestTimeout = TimeSpan.Zero;
+        public TimeSpan ConnectTimeout = TimeSpan.Zero;
         public bool UsePiplining { get; set; }
         public RedisPipelineConfiguration Pipeline { get; set; }
     }
