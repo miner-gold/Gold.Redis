a8c7b701f21e916f0521d493fff998a56f9532b0
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-11-23 22:59:16 +0200

    Change the Socket container class to be a wrapper class
    This allows us to mock the socket class for testing

--- src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/SocketContainer.cs
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Communication/SocketContainer.cs
@@ -1,10 +1,25 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Collections;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Gold.Redis.Common.Configuration;
 using Gold.Redis.LowLevelClient.Interfaces;
+using Gold.Redis.LowLevelClient.Interfaces.Communication;
 
 namespace Gold.Redis.LowLevelClient.Communication
 {
     public class SocketContainer : ISocketContainer
     {
+        public async Task ConnectAsync(string host, int port)
+        {
+            await Socket.ConnectAsync(host, port);
+        }
+
+        public async Task<int> SendAsync(ArraySegment<byte> bytes, SocketFlags flag)
+        {
+            return await Socket.SendAsync(bytes, flag);
+        }
+
         public Socket Socket { get; }
         public IConnectionsContainer ConnectionsContainer { get; set; }
 
@@ -14,9 +29,19 @@ namespace Gold.Redis.LowLevelClient.Communication
             ConnectionsContainer = connectionsContainer;
         }
 
+        public SocketContainer(IConnectionsContainer connectionsContainer)
+        {
+            Socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            ConnectionsContainer = connectionsContainer;
+        }
+          
         public void Dispose()
         {
-            ConnectionsContainer.FreeSocket(Socket);
+            ConnectionsContainer.FreeSocket(this);
         }
+
     }
 }
---
+++ src/Gold.Redis/Gold.Redis.LowLevelClient/Interfaces/Communication/ISocketContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Gold.Redis.LowLevelClient.Interfaces.Communication
+{
+    public interface ISocketContainer : IDisposable
+    {
+        Task ConnectAsync(string host, int port);
+        Task<int> SendAsync(ArraySegment<byte> bytes, SocketFlags flag);
+        Socket Socket { get; }
+    }
+}
