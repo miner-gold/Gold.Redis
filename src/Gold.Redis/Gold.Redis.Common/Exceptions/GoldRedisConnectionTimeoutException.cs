890d1c7eb8254bd87c7f2bd85362ea7c46513486
Tomer Goldberg <tomergoldbergpsn@gmail.com>
2019-11-23 22:54:58 +0200

    Created new exceptions
    Created custom exceptions to the ConnectionTimeout and the RequestTimeout

---
+++ src/Gold.Redis/Gold.Redis.Common/Exceptions/GoldRedisConnectionTimeoutException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gold.Redis.Common.Exceptions
+{
+    public class GoldRedisConnectionTimeoutException : Exception
+    {
+        public GoldRedisConnectionTimeoutException(string host, int port,int maxRetryCount, TimeSpan maxConnectTimeout): 
+            base($"Could not connect to redis server.\nHost = {host}, port = {port}, Number of connection attempts = {maxRetryCount}." +
+                 $"\nConnection timed-out after {maxConnectTimeout}")
+        {
+
+        }
+    }
+}
